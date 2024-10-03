b1677a16ce234e8871d44d9de07c6a41381cb97b
ZLYF3R <mbonfantem@gmail.com>
2024-10-03 14:03:13 -0500

    Sout de lanzamiento

--- src/Desiciones.java
+++ src/Desiciones.java
@@ -1,5 +1,15 @@
 public class Desiciones {
+
     public static void main(String[] args) {
-        System.out.println("Hola");
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento >= 2022) {
+            System.out.println("La película es nueva");
+        } else {
+            System.out.println("Es un clásico de ayer y hoy");
+        }
+
     }
 }
