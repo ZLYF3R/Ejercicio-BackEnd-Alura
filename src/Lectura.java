4152720acd0f553c35217c46c0914cc75fc4f7f6
ZLYF3R <mbonfantem@gmail.com>
2024-10-24 14:04:36 -0500

    Creación de Scanner y Loops

--- src/Lectura.java
+++ src/Lectura.java
@@ -1,4 +1,20 @@
 package PACKAGE_NAME;
 
+import java.util.Scanner;
+
 public class Lectura {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe el nombre de tu película favorita");
+        String pelicula = teclado.nextLine();
+        System.out.println("Ahora escribe el año de lanzamiento");
+        int fechaDeLanzamiento = teclado.nextInt();
+        System.out.println("Por últimos, dinos que nota le das a esta película");
+        double notaDePelicula = teclado.nextDouble();
+
+        System.out.println(pelicula);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(notaDePelicula);
+
+    }
 }
--- src/Loop.java
+++ src/Loop.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
+import java.util.Scanner;
+
 public class Loop {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double nota = 0;
+        double mediaEvaluaciones = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Escribe la nota que le darías a la película Matrix");
+            nota = teclado.nextDouble();
+            mediaEvaluaciones = mediaEvaluaciones + nota;
+        }
+        System.out.println("La nota media para Matrix es: " + mediaEvaluaciones / 3);
+    }
 }
